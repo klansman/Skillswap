f9eafc8956a8347f36be9a1d81d4f9ea7e86ebe6
klansman <akachukwuvictor29@gmail.com>
2025-04-26 21:22:51 +0100

    Added model for skill swapping

--- users/models.py
+++ users/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 import uuid
+from django.conf import settings
 # Create your models here.
 
 class CustomUser(AbstractUser):
@@ -15,3 +16,14 @@ class Skill(models.Model):
 
     def __str__(self):
         return f"{self.name} by {self.user.username}"
+    
+class SwapRequest (models.Model):
+    sender = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='sent_requests')
+    reciever = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='recieved_requests')
+    sender_skill = models.ForeignKey(Skill, on_delete=models.CASCADE, related_name='offering_skill')
+    reciever_skill = models.ForeignKey(Skill, on_delete=models.CASCADE, related_name='recieving_skill')
+    status =  models.CharField(max_length=20, choices=[('pending', 'Pending'), ('accepted', 'Accepted'), ('rejected', 'Rejected')], default='pending')
+    created_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.sender} wants to swap {self.sender_skill} with {self.reciever_skill} of {self.reciever}"
